17bf4ba36b098d5e31bf0e7ad80f8658ea27f823
Ekojoecovenant <covenantekojoe@gmail.com>
2026-05-16 15:37:01 +0000

    chore: added user-settings entity file

---
+++ src/modules/user-settings/entities/user-settings.entity.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, JoinColumn, OneToOne } from 'typeorm';
+import { AbstractBaseEntity } from '../../../database/entities/abstract-base.entity';
+import { User } from '../../users/entities/user.entity';
+
+@Entity('user_settings')
+export class UserSettings extends AbstractBaseEntity {
+  @Column({ type: 'boolean' })
+  smsNotification: boolean;
+
+  @Column({ type: 'boolean' })
+  whatsappAlerts: boolean;
+
+  @Column({ type: 'boolean' })
+  criticalAlerts: boolean;
+
+  @Column({ type: 'boolean' })
+  AiLanguage: boolean;
+
+  @OneToOne(() => User, (user) => user.settings, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+}
