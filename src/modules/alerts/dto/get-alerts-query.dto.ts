8b81f324a8474f5647292d601afa5e476bc18bbc
OWK50GA <okoriewifrid.20201206203@futo.edu.ng>
2026-05-21 19:47:54 +0100

    fix: reduce exceptions to single string

--- src/modules/alerts/dto/get-alerts-query.dto.ts
+++ src/modules/alerts/dto/get-alerts-query.dto.ts
@@ -1,20 +1,24 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsInt, IsOptional, IsString, Min } from 'class-validator';
 import { AlertType } from '../../../common/enums';
+import { Type } from 'class-transformer';
 
 export class GetAlertsQueryDto {
   @ApiProperty({ example: 'low_battery' })
+  @IsOptional()
   @IsString()
   alert_type?: AlertType;
 
   @ApiProperty({ example: 2, nullable: true })
   @IsOptional()
+  @Type(() => Number)
   @IsInt()
   @Min(1)
   page_number?: number;
 
   @ApiProperty({ example: 10, nullable: true })
   @IsOptional()
+  @Type(() => Number)
   @IsInt()
   @Min(1)
   page_size: number;
