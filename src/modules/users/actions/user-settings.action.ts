aca1d438bf82ddeb73585bb82e9b181c42a28e72
Ekojoecovenant <covenantekojoe@gmail.com>
2026-05-22 20:18:15 +0000

    fix: fixed linting errors

--- src/modules/users/actions/user-settings.action.ts
+++ src/modules/users/actions/user-settings.action.ts
@@ -1,5 +1,5 @@
 import { AbstractModelAction } from '@hng-sdk/orm';
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { UserSettings } from '../entities/user-settings.entity';
